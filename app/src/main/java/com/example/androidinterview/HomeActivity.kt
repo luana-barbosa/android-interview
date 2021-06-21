f40d93c302c042a77c7f00f558ced43ec77e8ad0
luana-barbosa <luana_barbosa93@hotmail.com>
2021-06-21 14:22:05 -0300

    git configurations

---
+++ app/src/main/java/com/example/androidinterview/HomeActivity.kt
@@ -0,0 +1,19 @@
+package com.example.androidinterview
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.androidinterview.databinding.ActivityHomeBinding
+
+class HomeActivity : AppCompatActivity() {
+    lateinit var binding: ActivityHomeBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
---
+++ home/src/main/java/com/example/home/model/HomeModel.kt
@@ -0,0 +1,4 @@
+package com.example.home.model
+
+class HomeModel {
+}
